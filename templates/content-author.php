4d04a1fd99d617c7dd270b6b729347595c7abca7
helgatheviking <helgatheviking@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-31 20:10:33 +0000

    initial release
    git-svn-id: https://plugins.svn.wordpress.org/simple-user-listing/trunk@661797 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/content-author.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The Template for displaying No Results
+ *
+ * Override this template by copying it to yourtheme/authors/navigation-author.php
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+global $user;
+
+$user_info = get_userdata($user->ID);
+?>
+<div class="author-block">
+	<?php echo get_avatar( $user->ID, 90 ); ?>
+	<h2><a href="<?php echo get_author_posts_url($user->ID); ?>"><?php echo $user_info->display_name; ?></a> - <?php printf( __( '%s posts', 'simple-user-listing'), count_user_posts( $user->ID ) ); ?></h2>
+	<p><?php echo $user_info->description; ?></p>
+
+	<p><a href="<?php echo get_author_posts_url($user->ID); ?> ">Read <?php echo $user_info->display_name; ?> posts</a></p>
+</div>
